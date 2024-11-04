7b5d0d0865314f57811ea70a342bdcad798a308c
hyejin Han <86100654+red2132@users.noreply.github.com>
2024-11-04 16:29:04 +0900

    드롭박스 프로젝트 초기 설정

---
+++ dropbox_clone_project/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "config/material-tailwind-theme-provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "dropbox clone project",
+  description: "드롭박스 클론 프로젝트",
+};
+
+export default function RootLayout({ children }) {
+  return (  
+    <html lang="en">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+          integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </head>
+      <ThemeProvider>
+        <body className={inter.className}>{children}</body>
+      </ThemeProvider>
+    </html>
+  );
+}
