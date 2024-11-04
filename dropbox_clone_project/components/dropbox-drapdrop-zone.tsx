b359a95243b7e4e6fb002610c6bd9144be1263d3
hyejin Han <86100654+red2132@users.noreply.github.com>
2024-11-04 18:38:37 +0900

    드래그 앤 드롭 구현

--- dropbox_clone_project/components/dropbox-drapdrop-zone.tsx
+++ dropbox_clone_project/components/dropbox-drapdrop-zone.tsx
@@ -1,14 +1,14 @@
 "use client";
 
+import { Spinner } from "@material-tailwind/react";
 import { Button } from "@material-tailwind/react";
 import { useMutation } from "@tanstack/react-query";
 import { uploadFile } from "actions/storageActions";
 import { queryClient } from "config/ReactQueryClientProvider";
-import { useRef } from "react";
+import { useCallback, useRef } from "react";
+import { useDropzone } from "react-dropzone";
 
 export default function DropboxDrapDropZone() {
-  const fileRef = useRef(null);
-
   // 캐시 날림
   const uploadImageMutation = useMutation({
     mutationFn: uploadFile,
@@ -18,25 +18,31 @@ export default function DropboxDrapDropZone() {
       });
     },
   });
+
+  // 드래그 앤 드롭 구현
+  const onDrop = useCallback(async (acceptedFiles) => {
+    const file = acceptedFiles?.[0];
+    if (file) {
+      const formData = new FormData();
+      formData.append("file", file);
+      const result = await uploadImageMutation.mutate(formData);
+      console.log(result);
+    }
+  }, []);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
   return (
-    <form
-      onSubmit={async (e) => {
-        e.preventDefault();
-        const file = fileRef.current.files?.[0];
-        if (file) {
-          const formData = new FormData();
-          formData.append("file", file);
-          const result = await uploadImageMutation.mutate(formData);
-          console.log(result);
-        }
-      }}
-      className="w-full py-20 border-4 border-dotted border-indigo-700 flex flex-col items-center justify-center"
+    <div
+      {...getRootProps()}
+      className="w-full py-20 border-4 border-dotted border-indigo-700 flex flex-col items-center justify-center cursor-pointer"
     >
-      <input type="file" ref={fileRef} className="" />
-      <p>이미지 파일을 여기에 끌어다 주세요</p>
-      <Button loading={uploadImageMutation.isPending} type="submit">
-        파일 업로드
-      </Button>
-    </form>
+      <input {...getInputProps()} />
+      {uploadImageMutation.isPending ? (
+        <Spinner />
+      ) : isDragActive ? (
+        <p>이미지 파일을 여기에 끌어다 주세요</p>
+      ) : (
+        <p>이미지 파일을 여기에 끌어다 놓거나 클릭해 업로드하세요</p>
+      )}
+    </div>
   );
 }
