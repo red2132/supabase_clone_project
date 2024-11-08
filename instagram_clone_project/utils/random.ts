be5be49159eb22c91097ab5e5d8bf8dc755233ae
hyejin Han <86100654+red2132@users.noreply.github.com>
2024-11-08 12:37:04 +0900

    채팅창 UI 구현

---
+++ instagram_clone_project/utils/random.ts
@@ -0,0 +1,4 @@
+// 랜덤 사람 이미지
+export const getRandomImage = (index: number): string => {
+  return `https://randomuser.me/api/portraits/med/men/${index}.jpg`;
+};
--- instagram_clone_project/utils/recoil/atoms.ts
+++ instagram_clone_project/utils/recoil/atoms.ts
@@ -1,6 +1,6 @@
 import { atom } from "recoil";
 
-export const searchState = atom({
-  key: "searchState",
-  default: "",
+export const selectedIndexState = atom({
+  key: "selectedIndexState",
+  default: null,
 });
